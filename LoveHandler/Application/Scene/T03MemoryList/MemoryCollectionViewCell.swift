8fbe45dd28d7310f0ab3c0916d331de86fb6c2ff
TriBQ <10khongquang@gmail.com>
2021-06-11 23:08:49 +0700

    add waterfall collection viê

---
+++ LoveHandler/Application/Scene/T03MemoryList/MemoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryCollectionViewCell.swift
+//  LoveHandler
+//
+//  Created by LanNTH on 11/06/2021.
+//
+
+import UIKit
+
+class MemoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
--- LoveHandler/Application/Scene/T03MemoryList/T03MemoryListViewController.swift
+++ LoveHandler/Application/Scene/T03MemoryList/T03MemoryListViewController.swift
@@ -11,6 +11,7 @@ import Combine
 class T03MemoryListViewController: BaseViewController {
     
     @IBOutlet weak var addButton: RoundButton!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var viewModel: T03MemoryListViewModel?
     
@@ -28,6 +29,7 @@ class T03MemoryListViewController: BaseViewController {
         super.setupView()
         isBackButtonVisible = true
         isTitleVisible = true
+        setupCollectionView()
     }
     
     override func setupLocalizedString() {
@@ -51,5 +53,49 @@ class T03MemoryListViewController: BaseViewController {
     override func setupTheme() {
         super.setupTheme()
         addButton.backgroundColor = Colors.hotPink
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        setupCollectionViewCell()
+        setupCollectionViewLayout()
+    }
+    
+    private func setupCollectionViewCell() {
+        let collectionViewNib = UINib(nibName: MemoryCollectionViewCell.className, bundle: nil)
+        collectionView.register(collectionViewNib, forCellWithReuseIdentifier: MemoryCollectionViewCell.className)
+    }
+    
+    private func setupCollectionViewLayout() {
+        let layout = CHTCollectionViewWaterfallLayout()
+        layout.minimumColumnSpacing = 3.0
+        layout.minimumInteritemSpacing = 3.0
+        self.collectionView.collectionViewLayout = layout
+    }
+}
+
+extension T03MemoryListViewController: CHTCollectionViewDelegateWaterfallLayout  {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: Int.random(in: 250..<500))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, columnCountFor section: Int) -> Int {
+        return 3
+    }
+}
+
+extension T03MemoryListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemoryCollectionViewCell.className, for: indexPath) as? MemoryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.imageView.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 500
     }
 }
