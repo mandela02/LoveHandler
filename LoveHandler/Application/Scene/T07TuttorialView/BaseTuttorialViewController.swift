5e3b31676d0574e3192a014224c1e09c61a8f3d6
TriBQ <10khongquang@gmail.com>
2021-07-03 17:43:28 +0700

    save setting

---
+++ LoveHandler/Application/Scene/T07TuttorialView/BaseTuttorialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseTuttorialViewController.swift
+//  LoveHandler
+//
+//  Created by LanNTH on 03/07/2021.
+//
+
+import UIKit
+
+class BaseTuttorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
